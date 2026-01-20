91010c954664dfc93aa953ea2897cf9f49708f39
austinbeckstead <austinbeckstead314@gmail.com>
2026-01-20 16:42:41 -0700

    Added test helper functions expectValidJwt, randomName, createAdminUser

---
+++ tests/user.js
@@ -0,0 +1,13 @@
+const { Role, DB } = require('../src/database/database.js');
+const {randomName } = require('./randomName.js');
+
+async function createAdminUser() {
+  let user = { password: 'toomanysecrets', roles: [{ role: Role.Admin }] };
+  user.name = randomName();
+  user.email = user.name + '@admin.com';
+
+  user = await DB.addUser(user);
+  return { ...user, password: 'toomanysecrets' };
+}
+
+module.exports = { createAdminUser };
