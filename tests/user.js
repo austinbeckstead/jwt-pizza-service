b09c2b21d878b5888332693020c7664c8c3db87a
austinbeckstead <austinbeckstead314@gmail.com>
2026-01-22 01:10:28 -0700

    Added diner user to user tests util

--- tests/user.js
+++ tests/user.js
@@ -2,12 +2,22 @@ const { Role, DB } = require('../src/database/database.js');
 const {randomName } = require('./randomName.js');
 
 async function createAdminUser() {
-  let user = { password: 'toomanysecrets', roles: [{ role: Role.Admin }] };
+  let user = { password: 'adminPassword', roles: [{ role: Role.Admin }] };
   user.name = randomName();
-  user.email = user.name + '@admin.com';
+  user.email = user.name + '@jwt.com';
 
   user = await DB.addUser(user);
-  return { ...user, password: 'toomanysecrets' };
+  return { ...user, password: 'adminPassword' };
 }
 
-module.exports = { createAdminUser };
+async function createDinerUser() {
+    let user = { password: 'dinerPassword', roles: [{ role: Role.Diner }] };
+    user.name = randomName();
+    user.email = user.name + '@jwt.com';
+
+    user = await DB.addUser(user);
+    return { ...user, password: 'dinerPassword' };
+
+}
+
+module.exports = { createAdminUser, createDinerUser };
