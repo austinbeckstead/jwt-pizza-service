9193c243fe073f988a394ccbb3b0d6bd2d9c7fd7
austinbeckstead <austinbeckstead314@gmail.com>
2026-03-27 10:25:30 -0600

    Updated lint to exclude copied load Test Script

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
+  { ignores: ["loadTests/loginAndOrder.js"] },
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
   {
