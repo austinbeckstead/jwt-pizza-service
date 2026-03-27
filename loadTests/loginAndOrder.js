044b76d3857cb859d9b33fe922cb3dbdf42d7c5c
austinbeckstead <austinbeckstead314@gmail.com>
2026-03-27 10:03:44 -0600

    added load test script

---
+++ loadTests/loginAndOrder.js
@@ -0,0 +1,216 @@
+import { sleep, check, group, fail } from 'k6'
+import http from 'k6/http'
+import jsonpath from 'https://jslib.k6.io/jsonpath/1.0.2/index.js'
+
+export const options = {
+  cloud: {
+    distribution: { 'amazon:us:ashburn': { loadZone: 'amazon:us:ashburn', percent: 100 } },
+    apm: [],
+  },
+  thresholds: {},
+  scenarios: {
+    Scenario_1: {
+      executor: 'ramping-vus',
+      gracefulStop: '30s',
+      stages: [
+        { target: 5, duration: '30s' },
+        { target: 15, duration: '1m' },
+        { target: 10, duration: '30s' },
+        { target: 0, duration: '30s' },
+      ],
+      gracefulRampDown: '30s',
+      exec: 'scenario_1',
+    },
+    Imported_HAR: {
+      executor: 'ramping-vus',
+      gracefulStop: '30s',
+      stages: [
+        { target: 20, duration: '1m' },
+        { target: 20, duration: '3m30s' },
+        { target: 0, duration: '1m' },
+      ],
+      gracefulRampDown: '30s',
+      exec: 'imported_HAR',
+    },
+  },
+}
+
+// Scenario: Scenario_1 (executor: ramping-vus)
+
+export function scenario_1() {
+  let response
+
+  // Automatically added sleep
+  sleep(1)
+}
+
+// Scenario: Imported_HAR (executor: ramping-vus)
+
+export function imported_HAR() {
+  let response
+
+  const vars = {}
+
+  group('Login and order - https://pizza.austinbeckstead.click/', function () {
+    // Homepage
+    response = http.get('https://pizza.austinbeckstead.click/', {
+      headers: {
+        accept:
+          'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        'cache-control': 'max-age=0',
+        priority: 'u=0, i',
+        'sec-ch-ua': '"Not:A-Brand";v="99", "Google Chrome";v="145", "Chromium";v="145"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"macOS"',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+      },
+    })
+    sleep(8.8)
+
+    // Login
+    response = http.put(
+      'https://pizza-service.austinbeckstead.click/api/auth',
+      '{"email":"a@jwt.com","password":"admin"}',
+      {
+        headers: {
+          accept: '*/*',
+          'accept-encoding': 'gzip, deflate, br, zstd',
+          'accept-language': 'en-US,en;q=0.9',
+          'content-type': 'application/json',
+          origin: 'https://pizza.austinbeckstead.click',
+          priority: 'u=1, i',
+          'sec-ch-ua': '"Not:A-Brand";v="99", "Google Chrome";v="145", "Chromium";v="145"',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '"macOS"',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'same-site',
+        },
+      }
+    )
+    if (!check(response, { 'status equals 200': response => response.status.toString() === '200' })) {
+      console.log(response.body);
+      fail('Login was *not* 200');
+    }
+    vars['token1'] = jsonpath.query(response.json(), '$.token')[0]
+
+    sleep(6.2)
+
+    // Menu
+    response = http.get('https://pizza-service.austinbeckstead.click/api/order/menu', {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        authorization: `Bearer ${vars['token1']}`,
+        'content-type': 'application/json',
+        origin: 'https://pizza.austinbeckstead.click',
+        priority: 'u=1, i',
+        'sec-ch-ua': '"Not:A-Brand";v="99", "Google Chrome";v="145", "Chromium";v="145"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"macOS"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+      },
+    })
+
+    // Franchise
+    response = http.get(
+      'https://pizza-service.austinbeckstead.click/api/franchise?page=0&limit=20&name=*',
+      {
+        headers: {
+          accept: '*/*',
+          'accept-encoding': 'gzip, deflate, br, zstd',
+          'accept-language': 'en-US,en;q=0.9',
+          authorization: `Bearer ${vars['token1']}`,
+          'content-type': 'application/json',
+          origin: 'https://pizza.austinbeckstead.click',
+          priority: 'u=1, i',
+          'sec-ch-ua': '"Not:A-Brand";v="99", "Google Chrome";v="145", "Chromium";v="145"',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '"macOS"',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'same-site',
+        },
+      }
+    )
+    sleep(9.1)
+
+    response = http.get('https://pizza-service.austinbeckstead.click/api/user/me', {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        authorization: `Bearer ${vars['token1']}`,
+        'content-type': 'application/json',
+        origin: 'https://pizza.austinbeckstead.click',
+        priority: 'u=1, i',
+        'sec-ch-ua': '"Not:A-Brand";v="99", "Google Chrome";v="145", "Chromium";v="145"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"macOS"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+      },
+    })
+    sleep(1.1)
+
+    // Purchase pizza
+    response = http.post(
+      'https://pizza-service.austinbeckstead.click/api/order',
+      '{"items":[{"menuId":1,"description":"Veggie","price":0.0038}],"storeId":"1","franchiseId":1}',
+      {
+        headers: {
+          accept: '*/*',
+          'accept-encoding': 'gzip, deflate, br, zstd',
+          'accept-language': 'en-US,en;q=0.9',
+          authorization: `Bearer ${vars['token1']}`,
+          'content-type': 'application/json',
+          origin: 'https://pizza.austinbeckstead.click',
+          priority: 'u=1, i',
+          'sec-ch-ua': '"Not:A-Brand";v="99", "Google Chrome";v="145", "Chromium";v="145"',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '"macOS"',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'same-site',
+        },
+      }
+    )
+    sleep(1.8)
+    vars['jwt'] = jsonpath.query(response.json(), '$.jwt')[0]
+
+
+    // Verify pizza
+    response = http.post(
+      'https://pizza-factory.cs329.click/api/order/verify',
+      `{"jwt": ${vars['jwt']}}`,
+      {
+        headers: {
+          accept: '*/*',
+          'accept-encoding': 'gzip, deflate, br, zstd',
+          'accept-language': 'en-US,en;q=0.9',
+          authorization: `Bearer ${vars['token1']}`,
+          'content-type': 'application/json',
+          origin: 'https://pizza.austinbeckstead.click',
+          priority: 'u=1, i',
+          'sec-ch-ua': '"Not:A-Brand";v="99", "Google Chrome";v="145", "Chromium";v="145"',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '"macOS"',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'cross-site',
+          'sec-fetch-storage-access': 'active',
+        },
+      }
+    )
+  })
+}
